d55e6be091ab899cff7bff2f4c6683022fab0f26
mcabrerar9 <114280115+mcabrerar9@users.noreply.github.com>
2023-03-18 10:54:36 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,71 @@
+
+#include <iostream>
+using namespace std;
+/*
+int main()
+
+{
+    int n,i;
+    long double factorial;
+
+    cout << "Introduce un numero: ";
+    cin >> n;
+    factorial=1;
+    for(i=1;i<=n;i++)
+        factorial = factorial * i;
+    cout << endl << "Factorial de " << n << " es:" << factorial << endl;
+    system("pause");
+}*/
+
+
+
+/*
+int main() {
+    int n1=0,n2=1,n3,i,number;
+    cout<<"INGRESE EL NUMERO: ";
+    cin>>number;
+    cout<<n1<<" "<<n2<<" ";
+    for(i=2;i<number;++i)
+    {
+        n3=n1+n2;
+        cout<<n3<<" ";
+        n1=n2;
+        n2=n3;
+    }
+    return 0;
+}*/
+
+
+/*
+int main()
+{
+
+    int n, i;
+
+
+    cout << "\Por favor introduce un numero: ";
+    cin >> n;
+
+    if(n==1)
+    {
+        cout << "\nNo es primo" << endl;
+    }
+    else
+    {
+        i=2;
+        while(n%i!=0)
+        {
+            i++;
+
+        }
+
+
+        if(i==n)
+            cout << "\nPrimo" << endl;
+        else
+            cout << "\nNo es primo" << endl;
+    }
+    system("pause");
+}*/
+
+
